7bd7d1652de1a3ce8df88c08d212000d98622bd8
Temitope <topwhiz5@gmail.com>
2022-07-16 18:22:40 +0100

    second commit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure", (req, res)=>{
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('This app listens on port 3000')
 })
 
